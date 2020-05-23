b2dfe204e693fb58cdb1b9beb7f771ff8e927ff9
mugnimaestra <mugnimaestra2@gmail.com>
2020-05-23 22:54:12 +0700

    Updates

---
+++ precache-manifest.c71737c283e9801855219b408cda717d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a2b5223cab7c6a434cf379ad6532ca",
+    "url": "/furniture-finder/index.html"
+  },
+  {
+    "revision": "edb1549c8bb4d25251b1",
+    "url": "/furniture-finder/static/css/main.97e5b10f.chunk.css"
+  },
+  {
+    "revision": "742f607208f9b44fefeb",
+    "url": "/furniture-finder/static/js/2.4738b397.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/furniture-finder/static/js/2.4738b397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edb1549c8bb4d25251b1",
+    "url": "/furniture-finder/static/js/main.19c22700.chunk.js"
+  },
+  {
+    "revision": "d89f9228923468ebe698",
+    "url": "/furniture-finder/static/js/runtime-main.6cbe3c50.js"
+  }
+]);
